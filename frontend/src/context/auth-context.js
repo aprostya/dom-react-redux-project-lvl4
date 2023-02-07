377c58a6a81d8efea0c2c25f0c144106beb46c45
aprostya <aprostya@yandex.ru>
2023-02-07 22:16:40 +0600

    final fixes for step 3

--- frontend/src/context/auth-context.js
+++ frontend/src/context/auth-context.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { useNavigate } from "react-router-dom";
 import useHttp from '../hooks/use-http';
 import { useLocalStorageState } from '../hooks/use-local-storage';
